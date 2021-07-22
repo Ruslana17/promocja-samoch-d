bf2d6912a11996cc77809b5bb64f25f3fab40790
Ruslana17 <55513221+Ruslana17@users.noreply.github.com>
2021-07-22 09:57:50 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+document.querySelector('.weżudział').onmouseenter=()=>{
+    document.querySelector('.weżudział').style.background='#2B2A29';
+   
+    console.log();
+} 
+document.querySelector('.weżudział').onmouseleave=()=>{
+    document.querySelector('.weżudział').style.background='none';
+}
+
+
+document.querySelector('.wyślij').onmouseenter=()=>{
+    document.querySelector('.wyślij').style.background='#2B2A29';
+   
+    console.log();
+} 
+document.querySelector('.wyślij').onmouseleave=()=>{
+    document.querySelector('.wyślij').style.background='none';
+}
