cfb1942547fc9980452bc711ebd8407218a47da1
Ruslana17 <55513221+Ruslana17@users.noreply.github.com>
2021-07-29 12:03:37 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -1,18 +1,36 @@
-document.querySelector('.weżudział').onmouseenter=()=>{
+
+
+/* наведение и убирание мыши */
+  /* document.querySelector('.weżudział').onmouseenter=()=>{
     document.querySelector('.weżudział').style.background='#2B2A29';
-   
-    console.log();
-} 
-document.querySelector('.weżudział').onmouseleave=()=>{
+    } 
+    document.querySelector('.weżudział').onmouseleave=()=>{
     document.querySelector('.weżudział').style.background='none';
-}
+    }
 
-
-document.querySelector('.wyślij').onmouseenter=()=>{
+/* наведение и убирание мыши */
+    /*document.querySelector('.wyślij').onmouseenter=()=>{
     document.querySelector('.wyślij').style.background='#2B2A29';
-   
-    console.log();
-} 
-document.querySelector('.wyślij').onmouseleave=()=>{
-    document.querySelector('.wyślij').style.background='none';
-}
+    } 
+    document.querySelector('.wyślij').onmouseleave=()=>{
+    document.querySelector('.wyślij').style.background="none";
+    } 
+*/
+
+let mouse1 = document.querySelector('.weżudział'),
+    mouse2 = document.querySelector('.wyślij');
+
+mouse1.addEventListener('mouseenter', function(){
+mouse1.style.background='#2B2A29';
+});
+
+mouse1.addEventListener('mouseleave', function(){
+mouse1.style.background='none';
+});
+
+mouse2.addEventListener('mouseenter', function(){
+mouse2.style.background='#2B2A29';
+});
+mouse2.addEventListener('mouseleave', function(){
+mouse2.style.background='none';
+});
